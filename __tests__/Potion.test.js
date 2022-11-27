623bbca4708e5b30c277301c92f72e3186d7864e
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-11-26 20:29:03 -0500

    test and create Potion constructor

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -6,3 +6,12 @@ test('creates a health potion object', () => {
   expect(potion.name).toBe('health');
   expect(potion.value).toEqual(expect.any(Number));
 });
+
+// if a potion construction is called without any arguments, we will have it create a new potion with a random type and value
+test('creates a random potion object', () => {
+  const potion = new Potion();
+
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
+  expect(potion.value).toEqual(expect.any(Number));
+})
